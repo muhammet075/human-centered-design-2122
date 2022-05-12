2ee8b16b8161f4e679ae3ee03f72e5edc9670ad9
MUHAMMET KOMUR <muhammet075@icloud.com>
2022-05-12 10:02:03 +0200

    finished prototype

--- js/script.js
+++ js/script.js
@@ -1,68 +1,142 @@
 AOS.init({
-  offset: 300,
+    offset: 300,
 });
 
-var spreker1naam = "Woerdeman";
+
+var spreker1naam = "Hans"
 var spreker1 = document.getElementsByClassName("spreker1naam");
 for (let i = 0; i < spreker1.length; i++) {
-  spreker1[i].innerHTML = spreker1naam;
+    spreker1[i].innerHTML = spreker1naam;
 }
 
-var spreker2naam = "Tristan";
+var spreker2naam = "Tristan"
 var spreker2 = document.getElementsByClassName("spreker2naam");
 for (let i = 0; i < spreker2.length; i++) {
-  spreker2[i].innerHTML = spreker2naam;
+    spreker2[i].innerHTML = spreker2naam;
 }
+
+document.querySelector("#pauze").addEventListener("click", () => {
+    document.querySelector("#pauze").style.display = "none";
+    document.querySelector("#hervat").style.display = "block";
+});
+
+document.querySelector("#hervat").addEventListener("click", () => {
+    document.querySelector("#pauze").style.display = "block";
+    document.querySelector("#hervat").style.display = "none";
+});
+
 
 document.querySelector("#play").addEventListener("click", openPodcast);
 
 function openPodcast() {
-  document.querySelector("article").style.display = "none";
-  document.querySelector("#slomer").style.display = "none";
-  document.querySelector("header").scrollIntoView();
-  document.querySelector("#podcast").style.opacity = "1";
-  AOS.refresh();
-
-  var tween = gsap.to(window, {
-    duration: 200,
-    scrollTo: ".laatste",
-    ease: "none",
-    paused: "true",
-  });
-
-  tween.play();
-
-  document.querySelector("#sneller").addEventListener("click", toonSneller);
-
-  function toonSneller() {
-    var tween = gsap.to(window, {
-      duration: 80,
-      scrollTo: ".laatste",
-      ease: "none",
-      paused: "true",
-    });
-
-    document.querySelector("#sneller").style.display = "none";
-    document.querySelector("#slomer").style.display = "block";
-
-    tween.pause();
-    tween.resume();
-  }
-
-  document.querySelector("#slomer").addEventListener("click", toonSlomer);
-
-  function toonSlomer() {
-    var tween = gsap.to(window, {
-      duration: 200,
-      scrollTo: ".laatste",
-      ease: "none",
-      paused: "true",
-    });
-
-    document.querySelector("#sneller").style.display = "block";
+    document.querySelector("article").style.display = "none";
     document.querySelector("#slomer").style.display = "none";
+    document.querySelector("header").scrollIntoView();
+    document.querySelector("#podcast").style.opacity = "1";
+    AOS.refresh()
 
-    tween.pause();
-    tween.resume();
-  }
+    var tween = gsap.to(window, {
+        duration: 170,
+        scrollTo: ".laatste",
+        ease: "none",
+        paused: "true"
+    });
+
+    tween.play();
+
+    var indicator = TweenMax.to("#indicator", 170, {
+        bottom: 0,
+        ease: Power2.easeInOut,
+        yoyo: true,
+        paused: true
+    })
+
+    indicator.play();
+
+    document.querySelector("#pauze").addEventListener("click", () => {
+        tween.pause()
+        indicator.pause();
+    });
+
+    document.querySelector("#hervat").addEventListener("click", () => {
+        tween.resume();
+        indicator.play();
+    });
+
+
+
+    document.querySelector("#sneller").addEventListener("click", toonSneller);
+
+    function toonSneller() {
+        var tween = gsap.to(window, {
+            duration: 40,
+            scrollTo: ".laatste",
+            ease: "none",
+            paused: "true"
+        });
+
+        var indicator = TweenMax.to("#indicator", 40, {
+            bottom: 0,
+            ease: Power2.easeInOut,
+            yoyo: true,
+            paused: true
+        })
+        indicator.play();
+
+        document.querySelector("#sneller").style.display = "none";
+        document.querySelector("#slomer").style.display = "block";
+
+        tween.pause();
+        tween.resume();
+        document.querySelector("#pauze").addEventListener("click", () => {
+            tween.pause();
+            indicator.pause();
+        });
+        document.querySelector("#hervat").addEventListener("click", () => {
+            tween.resume(); indicator.resume;
+        });
+    }
+
+    document.querySelector("#slomer").addEventListener("click", toonSlomer);
+
+
+    function toonSlomer() {
+        var tween = gsap.to(window, {
+            duration: 170,
+            scrollTo: ".laatste",
+            ease: "none",
+            paused: "true"
+        });
+
+        var indicator = TweenMax.to("#indicator", 170, {
+            bottom: 0,
+            ease: Power2.easeInOut,
+            yoyo: true,
+            paused: true
+        })
+        indicator.play();
+
+        document.querySelector("#sneller").style.display = "block";
+        document.querySelector("#slomer").style.display = "none";
+
+        tween.pause();
+        tween.resume();
+        document.querySelector("#pauze").addEventListener("click", () => {
+            tween.pause()
+            indicator.pause();
+
+        });
+        document.querySelector("#hervat").addEventListener("click", () => {
+            tween.resume()
+            indicator.resume();
+        });
+    }
+
+    document.querySelector("#restart").addEventListener("click", () => {
+        tween.restart();
+        document.querySelector("#pauze").style.display = "block";
+        document.querySelector("#hervat").style.display = "none";
+    });
+
+
 }
